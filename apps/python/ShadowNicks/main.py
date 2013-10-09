f6d2bba42775ac0d68b59ffdcd6b46ca65f575ab
Richie Foreman <richie.foreman@gmail.com>
2013-10-09 12:28:54 -0400

    Code review feedback / Usage documentation

--- apps/python/ShadowNicks/main.py
+++ apps/python/ShadowNicks/main.py
@@ -18,26 +18,43 @@
    ITS DERIVATIVES.
    """
 
+"""
+    Allocates an alias on an alternate domain name for all users on a
+    given domain name.
+
+    This is useful for establishing "domain aliases" on secondary domains.
+
+    Setup:
+     - Create an OAuth2 Project and enable the Admin SDK
+     - Create a client id for "installed applications"
+     - Download the client_secrets.json file and place it in the same dir.
+     - Run the script for the first time to authorize the script.
+       - The script will need to be authorized as a Domain Administrator
+     - Credentials are stored permanently in an 'auth.dat' file.
+
+    Example:
+     python main.py --domain acmecorp.com --shadow g.acmecorp.com
+     (all users on acmecorp.com receive a g.acmecorp.com alias)
+     (e.g. bob@g.acmecorp.com -> bob@acmecorp.com)
+"""
+
 __author__ = 'richieforeman@google.com (Richie Foreman)'
 
 from argparse import ArgumentParser
+import httplib2
+from Queue import Queue
+import threading
 
 from apiclient.discovery import build
-
-import httplib2
-
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run_flow
 try:
+    # the documentation says to import his way...
     from oauth2client.tools import run_parser
 except ImportError:
-    # hmm, seems like a bug in oauth2client to me.
+    # .. but it doesn't work, so fall back to this.
     from oauth2client.tools import argparser as run_parser
-
-from Queue import Queue
-
-import threading
 
 SCOPES = [
     'https://www.googleapis.com/auth/admin.directory.user.readonly',
@@ -50,61 +67,12 @@ THREADS = 25
 
 http = httplib2.Http()
 
-def get_credentials(args):
-    storage = Storage(AUTH_STORAGE_FILE)
-    credentials = storage.get()
-
-    if not credentials or credentials.invalid:
-        flow = flow_from_clientsecrets(filename=CLIENT_SECRETS,
-                                       scope=SCOPES)
-
-        credentials = run_flow(flow=flow,
-                               storage=storage,
-                               flags=args,
-                               http=http)
-
-    return credentials
-
-def main(args):
-    credentials = get_credentials(args)
-
-    credentials.authorize(http)
-
-    # build the directory service
-    service = build(serviceName="admin",
-                    version="directory_v1",
-                    http=http)
-
-    # establish a queue which will drive our thread pool.
-    queue = Queue(maxsize=THREADS*4)
-
-    # prefork the threads.
-    print "Forking %d Processor Threads... " % THREADS,
-
-    for i in range(THREADS):
-        print ".",
-        UserShadowNickThread(queue=queue,
-                             credentials=credentials,
-                             args=args).start()
-    print ".. Done!"
-
-    # fetch user pages and push each user into the queue.
-    nextToken = ""
-    while nextToken is not None:
-        response = service.users().list(domain=args.domain,
-                                        pageToken=nextToken).execute()
-
-        for user in response['users']:
-            queue.put(user)
-
-        nextToken = response.get("nextPageToken", None)
-
-    print "Waiting for all threads to rejoin parent..."
-    queue.join()
-    print "All threads rejoined parent. Done!"
-
 
 class UserShadowNickThread(threading.Thread):
+    '''
+    Thread class that receives a queue of users
+    and applies a shadow nickname to each
+    '''
     daemon = True
 
     q = None
@@ -158,6 +126,62 @@ class UserShadowNickThread(threading.Thread):
                       "nick for user %s - Exists!" % email
 
             self.q.task_done()
+
+
+def get_credentials(args):
+    storage = Storage(AUTH_STORAGE_FILE)
+    credentials = storage.get()
+
+    if not credentials or credentials.invalid:
+        flow = flow_from_clientsecrets(filename=CLIENT_SECRETS,
+                                       scope=SCOPES)
+
+        credentials = run_flow(flow=flow,
+                               storage=storage,
+                               flags=args,
+                               http=http)
+
+    return credentials
+
+
+def main(args):
+    credentials = get_credentials(args)
+
+    credentials.authorize(http)
+
+    # build the directory service
+    service = build(serviceName="admin",
+                    version="directory_v1",
+                    http=http)
+
+    # establish a queue which will drive our thread pool.
+    queue = Queue(maxsize=THREADS*4)
+
+    # prefork the threads.
+    print "Forking %d Processor Threads... " % THREADS,
+
+    for i in range(THREADS):
+        print ".",
+        UserShadowNickThread(queue=queue,
+                             credentials=credentials,
+                             args=args).start()
+    print ".. Done!"
+
+    # fetch user pages and push each user into the queue.
+    nextToken = ""
+    while nextToken is not None:
+        response = service.users().list(domain=args.domain,
+                                        pageToken=nextToken).execute()
+
+        for user in response['users']:
+            if not user['suspended']:
+                queue.put(user)
+
+        nextToken = response.get("nextPageToken", None)
+
+    print "Waiting for all threads to rejoin parent..."
+    queue.join()
+    print "All threads rejoined parent. Done!"
 
 
 if __name__ == "__main__":
